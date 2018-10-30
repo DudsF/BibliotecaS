34b68393ad82499bcb3d3f27fe2f09d293624b9e
Win 10 <Win 10@10.128.1.254>
2018-10-30 16:21:03 -0300

    Resolvendo conlitos

--- src/daos/ConnectionFactory.java
+++ src/daos/ConnectionFactory.java
@@ -8,15 +8,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	
 	private static Connection con = null;
-<<<<<<< HEAD
 	private static final String banco = "bibliotecas";
-=======
-<<<<<<< HEAD
-	private static final String banco = "biblioteca";
-=======
-	private static final String banco = "biblioteca_lamary";
->>>>>>> branch 'master' of https://github.com/DudsF/BibliotecaS.git
->>>>>>> branch 'master' of https://github.com/DudsF/BibliotecaS.git
 	private static final String user = "root";
 	private static final String password = "lab02";
 
