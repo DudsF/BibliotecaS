25fb46bb3e55e002f11363fdbe08275572bf748b
20151074010611 <20151074010611@MC266466.ifrn.local>
2018-11-05 11:19:51 -0300

    NOOOOOOOOOOOOOOOVOS!

--- src/daos/EmprestimoDAO.java
+++ src/daos/EmprestimoDAO.java
@@ -1,5 +1,21 @@
 package daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import models.Emprestimo;
+
 public class EmprestimoDAO {
-	
-}
+	private Connection connection;
+
+	public EmprestimoDAO() {
+		connection = ConnectionFactory.getConnection();
+	}
+
+	public boolean inserir( Emprestimo emprestimo) {
+
+		String sql = "insert into emprestimo (matricula, titulo, dataEmprestimo, dataDevolucao) values (?, ?, ?, ?);";
+	}
+	try 
+
--- src/models/Emprestimo.java
+++ src/models/Emprestimo.java
@@ -1,5 +1,52 @@
 package models;
 
+import java.util.Calendar;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class Emprestimo {
 
+	private String matricula;
+	private String titulo;
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	private Calendar dataEmprestimo;
+	private Calendar dataDevolucao;
+
+	@Override
+	public String toString() {
+		return "aluno [matricula=" + matricula + ", titulo=" + titulo + ", dataEmprestimo=" + dataEmprestimo + ", dataDevolucao=" + dataDevolucao	+ "]";
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getTituloDoLivro() {
+		return titulo;
+	}
+
+	public void setTituloDoLivro(String tituloDoLivro) {
+		titulo = tituloDoLivro;
+	}
+
+	public Calendar getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+
+	public void setDataEmprestimo(Calendar dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public Calendar getDataDevolucao() {
+		return dataDevolucao;
+	}
+
+	public void setDataDevolucao(Calendar dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
+	}
+	
 }
